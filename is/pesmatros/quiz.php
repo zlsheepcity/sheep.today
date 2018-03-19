1c073ee38813b22c53fbdcd1553219d8ba501672
zlsheepcity <zl.sheepcity@gmail.com>
2018-03-19 16:20:10 +0200

    map clue

---
+++ is/pesmatros/quiz.php
@@ -0,0 +1,54 @@
+<div id="cover">
+
+<h1><small>Тест Пройти</small><br/>Какой вы<br/>Пёс-Матрос?</h1>
+
+<div id="form">
+<form action="./" method="get">
+	<input type="hidden" name="quiz" value="quiz"/>
+
+	<fieldset style="width:340px;">
+		<p>Ситуация: одинокая столетняя старушка очень хочет перейти улицу. Но у нее на руках двадцатикилограмовая кастрюля борща. В остальном все хорошо — солнышко светит, птички поют, воскресение. Как вы проведете свой выходной?</p>
+		
+		<ul>
+			<li><label><input type="radio" name="q1" value="1" checked="yes" /> съем 10 кг борща — <i>полегчает</i></label></li>
+			<li><label><input type="radio" name="q1" value="2"/> высплюсь как следует</label></li>
+			<li><label><input type="radio" name="q1" value="3"/> отправлюсь в путешествие</label></li>
+		</ul>
+	</fieldset>
+	
+	<fieldset>
+		<p>Ситуация: в комнате сидят четыре женщины и один мужчина. Каждая женщина вяжет и видит только одного мужчину. Сколько петель видит паучок сидящий в углу между первой и второй?</p>
+		
+		<ul>
+			<li><label><input type="radio" name="q2" value="1" checked="yes" /> одну единственную свою неповторимую</label></li>
+			<li><label><input type="radio" name="q2" value="2"/> не уверен</label></li>
+			<li><label><input type="radio" name="q2" value="3"/> паучок видит тысячи мух</label></li>
+		</ul>
+	</fieldset>	
+	
+	<fieldset>
+		<p>Ситуация: вы приходите к гадалке, а там шницель. Ферштейн? (ответ не будет влиять на результаты опроса<span class="red">*</span>)</p>
+		
+		<ul>
+			<li><label><input type="radio" name="q3" value="1" checked="yes" /> должно быть не более, чем было под Измаилом</label></li>
+			<li><label><input type="radio" name="q3" value="2"/> чувство долга</label></li>
+			<li><label><input type="radio" name="q3" value="3"/> КЕЖМАН КЕЖМАН КЕЖМАН</label></li>
+		</ul>
+		<small><span class="red">*</span> может быть</small>
+	</fieldset>	
+
+	<fieldset>
+		<p>Ситуация: в компании лесных зверюшек вы делили апельсин. Вас много, а апельсин только один. Номинально он принадлежит белке, кому он будет принадлежать через 15 минут?</p>
+		
+		<ul>
+			<li><label><input type="radio" name="q4" value="1" checked="yes" /> самому сильному еноту</label></li>
+			<li><label><input type="radio" name="q4" value="2"/> земле, а потом вырастет апельсиновое дерево и плодов всем хватит</label></li>
+			<li><label><input type="radio" name="q4" value="3"/> хватит уже</label></li>
+		</ul>
+	</fieldset>		
+	
+	<fieldset><input type="submit" value="Какой я Пёс-Матрос?"/></fieldset>
+
+</form>
+</div>
+</div>
